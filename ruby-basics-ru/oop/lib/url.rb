a002035a95e2ed78722504f408b33f787fea42d7
@hexlet/cli <support@hexlet.io>
2023-12-28 03:33:22 +0300

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -11,7 +11,7 @@ class Url
   include Comparable
   
   attr_accessor :url
-  def_delegators :url, :host, :scheme, :query
+  def_delegators :url, :host, :scheme, :query, :port
 
   def initialize url
     @url = URI(url)
@@ -25,8 +25,9 @@ class Url
   end
 
   def query_param key, value = nil
-    params = query_params
-    return query_params[key] if value.nil?
+    query_params
+    return query_params[key] if value.nil? # геттер
+    return query_params[key] if query_params.key?(key) # когда ключ существует, но передали новый параметр
 
     new_params = param_pairs
       .reduce([]) { |acc, (key, value)| acc << "#{key}=#{value}" }
@@ -38,6 +39,7 @@ class Url
   def ==(other)
     # self <=>(other)
     host == other.host &&
+    port == other.port &&
     scheme == other.scheme &&
     query_params == other.query_params
   end
@@ -51,24 +53,25 @@ class Url
   # end
 
   def param_pairs
+    return [] if query.nil?
     query.split('&')
   end
 
 end
 
-yandex_url = Url.new 'http://yandex.ru?key=value&key2=value2'
-puts yandex_url.host # yandex.ru
-puts yandex_url.scheme # http
-puts yandex_url.query_params # { key: 'value', key2: 'value2' }
-puts yandex_url.query_param(:key) # 'value'
-puts yandex_url.query_param(:key2, 'lala') # 'value2'
-puts yandex_url.query_param(:new, 'ehu') # 'ehu'
-puts yandex_url.query_param(:lalala) # nil
-puts yandex_url.query_param(:lalala, 'default') # 'default'
+# yandex_url = Url.new 'http://yandex.ru:80?key=value&key2=value2'
+# puts yandex_url.host # yandex.ru
+# puts yandex_url.scheme # http
+# puts yandex_url.query_params # { key: 'value', key2: 'value2' }
+# puts yandex_url.query_param(:key) # 'value'
+# puts yandex_url.query_param(:key2, 'lala') # 'value2'
+# puts yandex_url.query_param(:new, 'ehu') # 'ehu'
+# puts yandex_url.query_param(:lalala) # nil
+# puts yandex_url.query_param(:lalala, 'default') # 'default'
 
-google_url = Url.new 'https://google.com:80?a=b&c=d&lala=value'
-puts yandex_url == google_url # false
+# google_url = Url.new 'https://google.com:80?a=b&c=d&lala=value'
+# puts yandex_url == google_url # false
 
-yandex_url_same = Url.new 'http://yandex.ru?key2=value2&key=value'
-puts yandex_url == yandex_url_same # true
+# yandex_url_same = Url.new 'http://yandex.ru?key2=value2&key=value'
+# puts yandex_url == yandex_url_same # true
 # END
