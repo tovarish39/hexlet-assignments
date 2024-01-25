af544b675bd45bc54295b43e5c286fb71d65ae02
@hexlet/cli <support@hexlet.io>
2024-01-25 22:29:43 +0200

    submit rails-basics-ru/relations

---
+++ rails-basics-ru/relations/app/models/task.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  status_id   :integer
+#  user_id     :integer
+#
+# Indexes
+#
+#  index_tasks_on_status_id  (status_id)
+#  index_tasks_on_user_id    (user_id)
+#
+class Task < ApplicationRecord
+  belongs_to :user
+  belongs_to :status
+
+  validates :name, presence: true
+  # validates :name, :description, presence: true
+end
