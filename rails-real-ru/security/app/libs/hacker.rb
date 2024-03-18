2a83925aebc62caf06f36d57dc8e409daaa70480
@hexlet/cli <support@hexlet.io>
2024-03-18 15:36:07 +0200

    submit rails-real-ru/security

---
+++ rails-real-ru/security/app/libs/hacker.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'faraday'
+require 'nokogiri'
+
+class Hacker
+  class << self
+    def hack(email, password)
+      # BEGIN
+      true
+      # END
+    end
+  end
+end
